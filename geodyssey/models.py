4efbd52aeb77af5568a21e0b58f6eac1e1de579b
Jeremy Bowers <jeremyjbowers@gmail.com>
2012-05-23 13:54:24 -0400

    Blanks and nulls, friends.

--- geodyssey/models.py
+++ geodyssey/models.py
@@ -5,11 +5,11 @@ class GeographicShape(models.Model):
     A simple ABC for storing geographic shapes, e.g., polygons.
     """
     name                    = models.CharField(max_length=255)
-    administrative_level    = models.IntegerField()
-    administrative_type     = models.CharField(max_length=255)
-    area                    = models.FloatField()
-    area_description        = models.CharField(max_length=255)
-    fips                    = models.CharField(max_length=255)
+    administrative_level    = models.IntegerField(blank=True, null=True)
+    administrative_type     = models.CharField(max_length=255, blank=True, null=True)
+    area                    = models.FloatField(blank=True, null=True)
+    area_description        = models.CharField(max_length=255, blank=True, null=True)
+    fips                    = models.CharField(max_length=255, blank=True, null=True)
 
     poly            = models.PolygonField(srid=4326)
 
@@ -24,10 +24,10 @@ class GeographicEvent(models.Model):
     """
     name                    = models.CharField(max_length=255)
     start_date_time         = models.DateTimeField()
-    end_date_time           = models.DateTimeField()
-    point                   = models.PointField()
+    end_date_time           = models.DateTimeField(blank=True, null=True)
+    point                   = models.PointField(blank=True, null=True)
 
-    related_geographies     = models.ManyToManyField(GeographicShape)
+    related_geographies     = models.ManyToManyField(GeographicShape, blank=True, null=True)
 
     objects         = models.GeoManager()
 
